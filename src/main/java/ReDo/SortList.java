3d73660d2df1458eaca8fe60b329373bd044b04d
LJS121 <2581456426@qq.com>
2019-03-23 20:47:33 +0800

    删除无用import

--- src/main/java/ReDo/SortList.java
+++ src/main/java/ReDo/SortList.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SortList {
     public static void main(String[] args) {
