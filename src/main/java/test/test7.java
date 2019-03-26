608930a87bdb8c9f02ed6e456ece394c26b6aa4f
LJS121 <2581456426@qq.com>
2019-03-26 08:37:02 +0800

    周二早上家里写 --3.26

--- src/main/java/test/test7.java
+++ src/main/java/test/test7.java
@@ -11,5 +11,11 @@ public class test7 {
         list.add(1);
         System.out.println(list.toString());
 
+        String s = "nihao\nwomenshi\nhaopengyou duima?";
+        System.out.println(s);
+
+        String s1 = "nih";
+//        System.out.println(s1.charAt(3));
+        System.out.println(s1.substring(3)==null);
     }
 }
