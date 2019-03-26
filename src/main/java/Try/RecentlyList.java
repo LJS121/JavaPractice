73b6d03b1201e35d908d0604e4c4eab7727b7d10
LJS121 <2581456426@qq.com>
2019-03-26 15:19:37 +0800

    3.26号公司完成

--- src/main/java/Try/RecentlyList.java
+++ src/main/java/Try/RecentlyList.java
@@ -1,16 +1,24 @@
 package Try;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class RecentlyList {
     List<String> list = new ArrayList<>();
     int capacity = 0;
+
     public RecentlyList(){}
+
     public RecentlyList(int capacity){
         this.capacity = capacity;
     }
-    public void add(String str){
+
+    public void add(String str) throws Exception {
+        if(str.equals("")){
+           throw new Exception("不能输入空字符串！");
+        }
         if(capacity>0){
             if(list.size()==capacity){
                 if(list.contains(str)){
@@ -38,7 +46,14 @@ public class RecentlyList {
         }
 
     }
+
     public String find(int index){
-        return list.get(index);
+        //因为要求是最近添加的作为第一个 最远添加的是最后一个 所以遍历的时候倒序遍历
+        return list.get(list.size()-1-index);
     }
+
+    public int size(){
+        return list.size();
+    }
+
 }
