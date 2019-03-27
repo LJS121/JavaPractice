fd06d062fc49e1bafaa780dd91ffc6fb205c1996
LJS121 <2581456426@qq.com>
2019-03-27 09:08:51 +0800

    周三早上家里写 --3.27

--- src/main/java/Try/ZeckendorfNumber.java
+++ src/main/java/Try/ZeckendorfNumber.java
@@ -6,21 +6,23 @@ public class ZeckendorfNumber {
 
     public void zeckendorfNumberSolution(){
         int[] num = {13,8,5,3,2,1};
-        for (int i = 0; i <= 20; i++) {
-            String result = "";
-            compute(i,num,result);
-            System.out.println(result);
-        }
-    }
-
-    public void compute(int i,int[] num,String result){
-        for (int j = 0; j < num.length; j=j+2) {
-            if(i<num[j]){
-                j--;
-                continue;
+        for(int i=0;i<=30;i++){
+            int[] flag = {0,0,0,0,0,0};
+            int count = i;
+            for (int j = 0; j < num.length; j++) {
+                if(num[j]>count){
+                    continue;
+                }else{
+                    flag[j]=1;
+                    count -= num[j];
+                    j++;
+                }
             }
-            result += 1*num[j];
-            compute(i,num,result);
+            String s = "";
+            for (int k = 0; k < num.length; k++) {
+                s += k!=num.length-1?flag[k]+"*"+num[k]+" + ":flag[k]+"*"+num[k];
+            }
+            System.out.println(s+" = "+i);
         }
     }
 
