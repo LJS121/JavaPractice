a866787a625a91e7b80f96696710b65bb0046f72
LJS121 <2581456426@qq.com>
2019-03-27 14:54:50 +0800

    3.27号公司完成

---
+++ src/main/java/LeetCode/ContainWater.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class ContainWater {
+    public int maxArea(int[] num){
+        int maxArea = 0;
+        for (int i = 0; i < num.length-1; i++) {
+            for (int j = i+1; j < num.length; j++) {
+                int nowArea = (j-i)*(num[i]>num[j]?num[j]:num[i]);
+                if(nowArea>maxArea){
+                    maxArea = nowArea;
+                }
+            }
+        }
+        return maxArea;
+    }
+
+}
