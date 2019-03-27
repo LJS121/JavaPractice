9ee20900051da93416d1560ea6c413d97b8e10c4
LJS121 <2581456426@qq.com>
2019-03-27 13:36:25 +0800

    3.27号公司完成

---
+++ src/main/java/LeetCode/TwoSum3.java
@@ -0,0 +1,25 @@
+package LeetCode;
+/**
+ * 肯定比 双层for循环 快 求 TwoSum的 3.0 版本
+ */
+
+import java.util.Arrays;
+
+public class TwoSum3 {
+    public int[] twoSum(int[] num,int target){
+        int i = 0;
+        int j = num.length-1;
+        Arrays.sort(num);
+        while(i<j){
+            int sum = num[i]+num[j];
+            if(sum>target){
+                j--;
+            }else if(sum<target){
+                i++;
+            }else{
+                return new int[]{i,j};
+            }
+        }
+        return new int[]{};
+    }
+}
