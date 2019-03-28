07e8a1ef367c040c34dabde213fd31c73c5aa1ce
LJS121 <251456426@qq.com>
2019-03-28 15:00:12 +0800

    3.28公司完成

---
+++ src/main/java/Try/HarryPotter3.java
@@ -0,0 +1,82 @@
+package Try;
+
+import com.google.common.primitives.Ints;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author jinsong.Liang
+ * @date 2019/3/28 11:05
+ */
+public class HarryPotter3 {
+    int price = 8;//欧元
+    public double harryPotter(int[] nums){//传入每本书购买数量的数组
+        if(nums.length!=5){
+            System.out.println("请你输入五本书购买数量的数组");
+            return 0;
+        }
+        List<Integer> input = Ints.asList(nums);
+        Collections.sort(input);
+        Collections.reverse(input);//我想要倒序排列的
+        int[] count = new int[nums.length];
+        for (int i = 0; i < input.size(); i++) {
+            count[i] = input.get(i);
+        }
+        //分别给每个list赋值
+        int count5 = 0,count3 = 0;
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < input.get(0); i++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int j = 0; j < count.length; j++) {
+                if(count[j]-->0){
+                    temp.add(1);
+                }
+            }
+            list.add(temp);
+            if(temp.size()==5){
+                count5++;
+            }
+            if(temp.size()==3){
+                count3++;
+            }
+        }
+
+        //5 3 变成 4 4
+        int min = count3>count5?count5:count3;
+        double change = min*(5*price*0.75+3*price*0.9-4*price*0.8*2);
+        double origin = calculateAll(list);
+        return origin-change;
+    }
+    public double calculateAll(List<List<Integer>> listAll){
+        double sum = 0;
+        for (List<Integer> list:listAll
+             ) {
+            switch(list.size()){
+                case 5:
+                    sum += list.size()*price*0.75;
+                    continue;
+                case 4:
+                    sum += list.size()*price*0.80;
+                    continue;
+                case 3:
+                    sum += list.size()*price*0.9;
+                    continue;
+                case 2:
+                    sum += list.size()*price*0.95;
+                    continue;
+                default:
+                    sum += list.size()*price;
+            }
+        }
+        return sum;
+    }
+
+    @Test
+    public void test(){
+        int[] nums = {1,3,2,4,5};
+        System.out.println(harryPotter(nums));
+    }
+}
--- src/main/java/test/test13.java
+++ src/main/java/test/test13.java
@@ -1,12 +1,20 @@
 package test;
+/**
+ * 没有赋值的String 类型被当作 ""空串处理
+ * 没有赋值的String[] 当作null处理
+ */
 
 import java.util.Arrays;
 
 public class test13 {
     public static void main(String[] args) {
-        int[] arr = {1,3,4,5};
-        int[] arr2 = {2,3,4,5};
-        arr = arr2;
-        System.out.println(Arrays.toString(arr));
+//        int[] arr = {1,3,4,5};
+//        int[] arr2 = {2,3,4,5};
+//        arr = arr2;
+//        System.out.println(Arrays.toString(arr));
+        String[] s = new String[3];
+        System.out.println(s[0]==null);
+        String ss = new String();
+        System.out.println(ss.equals(""));
     }
 }
