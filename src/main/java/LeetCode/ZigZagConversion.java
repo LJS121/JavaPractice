7e175308ec0db464c244213173d4fe1aba7bb090
LJS121 <251456426@qq.com>
2019-03-28 19:59:03 +0800

    3.28部分内容修改

---
+++ src/main/java/LeetCode/ZigZagConversion.java
@@ -0,0 +1,25 @@
+//package LeetCode;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+///**
+// * @author jinsong.Liang
+// * @date 2019/3/28 19:23
+// */
+//public class ZigZagConversion {
+//    public String convert(String s,int numRows){
+//        Map<Integer, List<String>> map = new HashMap<>();
+//        for (int j = 0; j < numRows;) {
+//            List<String> temp = new ArrayList<>();
+//            map.put(j,temp);
+//        }
+//        for (int i = 0; i < s.length(); i++) {
+//            if(i%3==0){
+//
+//            }
+//        }
+//    }
+//}
