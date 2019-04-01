81dfad4ab81bc93a596be1faa66796eedde79074
LJS121 <251456426@qq.com>
2019-04-01 10:50:21 +0800

    4.1 --company commit

--- src/main/java/leetcode/ReverseInteger.java
+++ src/main/java/leetcode/ReverseInteger.java
@@ -1,25 +1,32 @@
 package leetcode;
+/**
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1].
+ * For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+ */
 
 import org.junit.Test;
 
 public class ReverseInteger {
     public int reverse(int x) {
         int temp = Math.abs(x);
-        int result = 0;
+        long result = 0;
         while (temp / 10 > 0) {
             result = (result + temp % 10) * 10;
             temp /= 10;
         }
         result += temp;
-        if(x<0){
-            return -result;
+        if(result>Integer.MAX_VALUE||result<=Integer.MIN_VALUE){
+            return 0;
         }else{
-            return result;
+            return x>0?(int)result:(int)-result;
         }
     }
     @Test
     public void test(){
         System.out.println(reverse(1534236469));
         System.out.println(reverse(-123));
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
     }
 }
